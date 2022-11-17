d5842cc2c541edffdb02d3da1c0eb7ac47c2d42e
leineveber <egor.leineveber@altence.com>
2022-11-17 16:25:18 +0300

    feat(app): add screens

---
+++ src/features/account/screens/account.screen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const AccountScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>account</Text>
+    </View>
+  );
+};
---
+++ src/features/bikes-list/screens/bikes-list.screen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const BikesListScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>bikes list</Text>
+    </View>
+  );
+};
